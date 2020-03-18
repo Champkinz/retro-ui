25fd567e71beb11c9ae91d37506b41b34270ff32
Charana Amarasekara <danu_lak@live.com>
2020-03-19 00:35:06 +0530

    Updates

---
+++ precache-manifest.4d586dec854126e212ee99aa20c7136c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb37b56024a8c52b769f428e49908792",
+    "url": "/Champkinz/retro-ui/index.html"
+  },
+  {
+    "revision": "f2c8cf9d64a49462b5cc",
+    "url": "/Champkinz/retro-ui/static/css/main.fe5c6406.chunk.css"
+  },
+  {
+    "revision": "6764fdc34e9792ee8134",
+    "url": "/Champkinz/retro-ui/static/js/2.4f08f39e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Champkinz/retro-ui/static/js/2.4f08f39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c8cf9d64a49462b5cc",
+    "url": "/Champkinz/retro-ui/static/js/main.b678c34a.chunk.js"
+  },
+  {
+    "revision": "b0f995ef21f32a830404",
+    "url": "/Champkinz/retro-ui/static/js/runtime-main.affd80a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Champkinz/retro-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
